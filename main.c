7c8ed867182c81d381e829a876efed9c58aa158e
Samuel Mueller <samuel@kiwisound.de>
2020-03-25 23:50:56 +0100

    only input function is working

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAXIN 100
 
-void input(char in);
+void input(char in[]);
 
 int main()
 {
-	char in[100];
-	input(in);
+	char usrin[MAXIN];
+	double stack[MAXIN];
+	int pointer, end, output, i;
+	char operator;
+	operator = 0;
+	end = 0;
+	pointer = 0;
+	while (!end) {
+		input(usrin);
+		stack[pointer] = atof(usrin);
+		printf("%f", stack[pointer]);
+		++pointer;
+		if ((operator=getchar()) == '+' && pointer >= 2) {
+			for (int i = (pointer-1); i >= 0; --i) {
+				output += (stack[i] + stack[i-1]);
+			}
+		}
+	}
+	return 0;
 }
 
-void input(char in) {
+void input(char in[]) {
 	int c, i;
 	i = 0;
-	while ((c=getchar()) != EOF) {
+	while ((c=getchar()) != '\n') {
 		in[i] = c;
 		++i;
 	}
