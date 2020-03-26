cdc2a146d8872a304d8c5857a4bd0d677060c84c
Samuel Mueller <samuel@kiwisound.de>
2020-03-26 16:36:48 +0100

    operator now working

--- main.c
+++ main.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXIN 100
-
+#define TRUE 1
+#define FALSE 0
 void input(char in[]);
-char get_operator(void);
+/*char get_operator(void);*/
 
 int main()
 {
 	char usrin[MAXIN];
 	double stack[MAXIN];
-	int pointer, end, output, i;
-	end = 0;
+	int pointer, end, i;
+	double output;
+	output = 0;
+	end = FALSE;
 	pointer = 0;
 	while (!end) {
 		input(usrin);
-		stack[pointer] = atof(usrin);
-		printf("%f", stack[pointer]);
-		++pointer;
-		if (get_operator() == '+' && pointer >= 2) {
-			for (int i = (pointer-1); i >= 0; --i) {
-				output += (stack[i] + stack[i-1]);
+		if (pointer >= 1 && usrin[0] == '+') {
+			for (i=0; i <= (pointer-1) ; ++i) {
+				printf("\nstack at %d: %f", i, stack[i]);
 			}
+			stack[pointer-2] = (stack[pointer-1] + stack[pointer-2]);
+			stack[pointer-1] = 0;
+			printf("\n\noutput: %f\n", stack[pointer-2]);
+		} else if (usrin[0] == '.') {
+			end = TRUE;
+		} else {
+			stack[pointer] = atof(usrin);
+			printf("%f\n", stack[pointer]);
+			++pointer;
 		}
 	}
 	return 0;
@@ -42,11 +51,11 @@ void input(char in[]) {
 		in[MAXIN - 1] = '\0';
 }
 
-char get_operator(void) {
+/*char get_operator(void) {
 	int i;
 	char operator, c;
 	while ((c=getchar()) != '\n' && (c=='+' || c== '-' || c== '*' || c=='/')) {
 		operator = c;
 	}
 	return operator;
-}
+}*/
